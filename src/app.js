872506eb5f877cb8268daebeebeca6afcc758c7e
Dean Roulston <Dean@Deans-MacBook-Pro.local>
2018-04-26 22:26:12 +0100

    reworking to genres api and including post endpoint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const routes = require('./routes');
 const app = express();
 
+app.use(express.json());
+
 app.use('/', routes, (req, res) => {
+    // This handles any endpoints not defined in routes
     res.send(404);
 })
 
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const routes = express.Router();
 
+const genres = [
+    {id: 1, genre: 'horror'}, 
+    {id: 2, genre: 'romance'},
+    {id: 3, genre: 'action'}
+];
+
 routes.get('/', (req, res) => {
     res.status(200).send('Home page called');
 });
 
-routes.get('/flights/:from', (req, res) => {
-    console.log(req.params);
-    res.status(200).send('requested flights to ' + req.params.from);
+routes.get('/api/genres', (req, res) => {
+    res.status(200).send(genres);
 });
+
+routes.post('/api/genres', (req, res) => {
+    const newGenre = req.body.genre;
+    console.log(req.body);
+    const genre = {
+        id: genres.length+1,
+        genre: newGenre
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
 
 module.exports = routes;
